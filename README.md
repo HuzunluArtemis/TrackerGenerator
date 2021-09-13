20e950391c2ab5631ae1028e2a82f332d5a86697
Hüzünlü Artemis [HuzunluArtemis] <84624971+HuzunluArtemis@users.noreply.github.com>
2021-09-14 01:06:29 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# TrackerGenerator - Torrent Tracker Generator
+
+🇹🇷 Basit bir torrent tracker oluşturucusu.
+
+🇬🇧 A simple torrent tracker generator.
+
+## License
+<details>
+    <summary><b>Click Here For Details</b></summary>
+  <br>
+  <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">
+  <img src="https://www.gnu.org/graphics/gplv3-127x51.png" alt="GNU GPLv3 Image">
+</a>
+<br><br>
+DeleteServiceMessagesBot is Free Software: You can use, study share and improve it at your
+will. Specifically you can redistribute and/or modify it under the terms of the 
+  <a href="https://www.gnu.org/licenses/gpl.html">GNU General Public License</a> 
+  as published by the Free Software Foundation, either version 3 of the License, 
+  or (at your option) any later version.
+</details>
+
---
+++ gettrackers_with_newline.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# https://github.com/HuzunluArtemis/TrackerGenerator
+
+import requests
+import os
+
+tracker_urls = [
+    "https://newtrackon.com/api/all",
+    "https://ngosang.github.io/trackerslist/trackers_all_http.txt",
+    "https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt",
+    "https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_exclude.txt",
+    "https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt",
+    "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt",
+    "https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt"]
+
+if os.path.isfile('./trackers_with_newline.txt') or os.path.exists('./trackers_with_newline.txt'):
+	os.remove('./trackers_with_newline.txt')
+
+all = ""
+for i in tracker_urls:
+    r = requests.get(i)
+    resp = r.text.replace('\n\n',", ")
+    resp = resp.replace('\n',', ')
+    all += resp
+all.rstrip(',')
+
+alllist = all.split(", ")
+alllist = list(dict.fromkeys(alllist))
+strall = '\n'.join([str(elem) for elem in alllist])
+
+with open('./trackers_with_newline.txt','w') as fd:
+    fd.write(strall)
